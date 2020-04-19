7260bae4b1d1073bb1994054d9bf7ec2417176e1
Chien  Tan <chien.tan16@imperial.ac.uk>
2020-04-19 16:46:19 +0800

    more data

---
+++ ReactData/5_No_Likes_min_10.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+import numpy as np
+
+
+cols = ['name', 'love_count', 'wow_count',
+        'haha_count', 'sad_count', 'angry_count']
+
+df = pd.read_csv(r"C:\Users\jorda\Desktop\FB-Data\name_msg_desc_links_like_reacts\name_msg_desc_links_like_reacts.csv",
+                 usecols=cols, encoding='utf16')
+
+df['react_sum'] = df['love_count'] + df['wow_count'] + \
+    df['haha_count'] + df['sad_count'] + df['angry_count']
+
+df = df[df['react_sum'] > 10]
+
+df = df.drop('react_sum', axis=1)
+
+df = df[df['name'].apply(lambda x: isinstance(x, str))]
+
+df_reacts = df.select_dtypes(include=[np.number])
+
+df_reacts = df_reacts.div(df_reacts.sum(axis=1), axis=0)
+df[df_reacts.columns] = df_reacts
+
+shuffled_df = df.sample(frac=1).reset_index(drop=True)
+
+shuffled_df.to_csv('./ReactData/5_No_Likes_min_10.csv',
+                   encoding='utf16', index=False)
