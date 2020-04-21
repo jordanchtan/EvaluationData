80605c3d1e2fc54147b1c119d9f42263622fb781
Chien  Tan <chien.tan16@imperial.ac.uk>
2020-04-21 20:14:31 +0800

    update emo

--- ExpressData/EmoBank_Writer_All.py
+++ ExpressData/EmoBank_Writer_All.py
@@ -8,4 +8,7 @@ meta = pd.read_csv(path + r'\meta.tsv', sep='\t', index_col=0)
 df = pd.merge(df_writer, df_raw, on='id',  how='left')
 df = pd.merge(df, meta, on='id',  how='left')
 
+df = df[df['text'].apply(lambda x: isinstance(x, str))]
+df = df.sample(frac=1).reset_index(drop=True)
+
 df.to_csv('EmoBank_Writer_All.csv', index=False)
